cb95ffe66fb5f039c9a8036ecd1db71c7b8f1afe
etobaga <64399794+etobaga@users.noreply.github.com>
2020-09-09 19:43:45 +0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {AddCommand} from '@angular/cli/commands/add-impl';
+import {AddComponent} from './add/add.component';
+import {ListComponent} from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AddComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
